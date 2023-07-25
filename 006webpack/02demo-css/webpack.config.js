0a19014e278e08cc49b6ce54658986763f02c1a1
mhtkl <mhpsy@proton.me>
2023-07-26 00:34:30 +0800

    webpack postcss-preset-env

--- 006webpack/02demo-css/webpack.config.js
+++ 006webpack/02demo-css/webpack.config.js
@@ -15,6 +15,19 @@ module.exports = {
                     // "css-loader",//这里如果只有这一个loader是搞不定的
                     { loader: "style-loader", options: {} },//执行的顺序居然是从后往前的!!!？？？
                     { loader: "css-loader", options: {} },//完整的写法
+                    {
+                        //事实上这个对象里面的内容可以抽出去
+                        //抽成postcss.config.js文件
+                        loader: "postcss-loader", options: {
+                            postcssOptions: {
+                                plugins: [
+                                    // require("autoprefixer"),// 这个用的比较少
+                                    require("postcss-preset-env")
+                                ]
+                            }
+                        }
+                    }
+
                 ]
             }
         ]
